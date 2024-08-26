75472274b9831e0fcadf86bbb3473fbc62b821d0
OtheyaTHEBOSS <96703925+OtheyaTHEBOSS@users.noreply.github.com>
2024-08-26 07:05:46 +0000

    changes

--- tk-video.py
+++ tk-video.py
@@ -5,19 +5,19 @@ root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack(fill=tk.BOTH, expand=True)
 
-instance = vlc.Instance("--no-video")
+instance = vlc.Instance()
 player = instance.media_player_new()
+media = instance.media_new('1.mp4')
+player.set_media(media)
 
 video_widget = tk.Label(frame)
 video_widget.pack(fill=tk.BOTH, expand=True)
 
-media = instance.media_new('1.mp4')
-player.set_media(media)
 player.set_hwnd(video_widget.winfo_id())
 
 player.play()
 
-text_label = tk.Label(frame, text="This is text over the video")
-text_label.pack()
+#text_label = tk.Label(frame, text="This is text over the video")
+#text_label.pack()
 
 root.mainloop()
